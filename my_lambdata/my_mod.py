c60b7744188c498bbac08eeae6e5f7da6f4a6dae
Subir Chowdhury <ssengupta801@gmail.com>
2020-05-06 20:57:17 -0500

    updating files

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -7,7 +7,6 @@ def enlarge(n):
 if __name__ == "__main__":
   #only run the code below if executed from command line
   # otherwise dont run it for eg if we are trying to import it 
- x = 5
- print(enlarge(x))    
+    
  y = int(input("Please choose a number(eg.5):"))
- print(enlarge(y))
+ print(y,enlarge(y))
