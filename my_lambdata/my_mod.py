b8ac7d9a38299fd62282d71324a289adbdc500ae
Subir Chowdhury <ssengupta801@gmail.com>
2020-05-06 19:59:23 -0500

    created init file

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,10 +1,13 @@
 
-
+#my_mod.py
 
 def enlarge(n):
     return n*100
 
-x = 5
-print(enlarge(x))    
-y = int(input("Please choose a number(eg.5):"))
-print(enlarge(y))
+if __name__ == "__main__":
+  #only run the code below if executed from command line
+  # otherwise dont run it for eg if we are trying to import it 
+ x = 5
+ print(enlarge(x))    
+ y = int(input("Please choose a number(eg.5):"))
+ print(enlarge(y))
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,7 +1,12 @@
-
+#my script.py
 
 from pandas import DataFrame
+#from my_mod import enlarge #this works
+from my_lambdata.my_mod import enlarge
+
 print("Hello")
 
 df = DataFrame({"a":[1,2,3],"b":[4,5,6]})
 print(df.head())
+x = 11
+print(enlarge(x))
