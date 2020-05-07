62324fc3fab247ebbfaf49dd6d5997dc356f094e
Subir Chowdhury <ssengupta801@gmail.com>
2020-05-07 13:02:46 -0500

    Final commits

--- lambdata-sweta-assignment1/sweta_assign1/mod_null.py
+++ lambdata-sweta-assignment1/sweta_assign1/mod_null.py
@@ -2,9 +2,15 @@
 import pandas as pd
 #from sweta_assign1.mod_null import date_null
 
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
-df=pd.DataFrame(data)
-
 def data_null(df):
    print(df.isnull().sum())
+
+
+
+if __name__ == "__main__":
+   data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+        'Age':[20, 21, 19, 18]}
+   df=pd.DataFrame(data)
+
+   data_null(df)
+
