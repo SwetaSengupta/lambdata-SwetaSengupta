a3f679f2cabb706b0c770f0cf9aebe57df7dcc59
Subir Chowdhury <ssengupta801@gmail.com>
2020-05-15 14:03:12 -0500

    updated dt_test.py file

--- lambdata-sweta-assignment1/test/dt_test.py
+++ lambdata-sweta-assignment1/test/dt_test.py
@@ -7,19 +7,19 @@ class test_dt(unittest.TestCase):
 
 # expect that it has three more column and the same number of rows, after we invoke the function
     # expect that certain column names exist before and certain col names exist after
-    
+    #expects certain values in certain columns
              ddata={"Date": ["02/15/2019", "02/16/2019", "02/17/2019", "02/18/2019"],"column2":["hey","hello","hi","namaste"]} 
              df = pd.DataFrame(ddata)
     
-    #self.assertEqual(list(df.columns), ['date',column2])
+    
 
              self.assertEqual(list(df.columns),['Date','column2'])
              result = split_dates(df,'Date')
              self.assertEqual(list(result.columns),['Date','column2','month','year','day'])
-            # assert result.iloc[0]["Date"] == "02/15/2019"
-            # assert result.iloc[0]["month"] == "2"
-            # assert result.iloc[0]["year"] == "2019"
-            # assert result.iloc[0]["day"] == "15"
+             self.assertEqual(result.iloc[0]["Date"] , "02/15/2019")
+             self.assertEqual( result.iloc[0]["month"], 2)
+             self.assertEqual(result.iloc[0]["year"], 2019)
+             self.assertEqual(result.iloc[0]["day"] , 15)
 
 if __name__=="__main__":
     unittest.main()
